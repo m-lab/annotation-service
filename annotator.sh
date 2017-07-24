6d6ba8e3ec37c2c9f76202bd612a4af209f8c37c
Shelby Chen <shelbychen@shelbychen.nyc.corp.google.com>
2017-07-24 13:06:47 -0400

    adding google application cridentials

--- annotator.sh
+++ annotator.sh
@@ -4,5 +4,5 @@ echo $sandbox_service_key | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 ls /home/travis/google-cloud-sdk/ 
 ls /home/travis/
-export APPENGINE_DEV_APPSERVER="${HOME}/google-cloud-sdk/bin/dev_appserver.py" && source "${HOME}/google-cloud-sdk/path.bash.inc" && go test -v ./...
+export GOOGLE_APPLICATION_CREDENTIALS="${HOME}/gcloud-service-key.json" && export APPENGINE_DEV_APPSERVER="${HOME}/google-cloud-sdk/bin/dev_appserver.py" && source "${HOME}/google-cloud-sdk/path.bash.inc" && go test -v ./...
 
