0148f8ee118ade5a8f4363ba97178007d16a93d1
Shelby Chen <shelbychen@shelbychen.nyc.corp.google.com>
2017-07-24 11:41:55 -0400

    add permisions for shell file

--- annotator.sh
+++ annotator.sh
@@ -5,3 +5,4 @@ gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 ls /home/travis/google-cloud-sdk/ 
 ls /home/travis/
 export APPENGINE_DEV_APPSERVER="${HOME}/google-cloud-sdk/bin/dev_appserver.py" && source "${HOME}/google-cloud-sdk/path.bash.inc" && go test -v ./...
+
