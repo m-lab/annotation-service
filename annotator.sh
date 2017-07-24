472ee8ef75383f4fa4ee3dd92bc9f9ed0f96ad3e
Shelby Chen <shelbychen@shelbychen.nyc.corp.google.com>
2017-07-24 12:54:22 -0400

    taking out dashes and replacing with underscore

--- annotator.sh
+++ annotator.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 $TRAVIS_BUILD_DIR/travis/install_gcloud.sh app-engine-python app-engine-go
-echo $sandbox-service-key | base64 --decode -i > ${HOME}/gcloud-service-key.json
+echo $sandbox_service_key | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 ls /home/travis/google-cloud-sdk/ 
 ls /home/travis/
