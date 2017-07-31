5c5342c4eb39d4062f78043186d9d632f6a3278a
Shelby Chen <shelbychen@shelbychen.nyc.corp.google.com>
2017-07-31 18:26:02 -0400

    add commentation for annotation.sh

--- annotator.sh
+++ annotator.sh
@@ -1,8 +1,9 @@
+#shell file allows App engine's devappserver to override google devappserver and handles token issues 
+#must be done in the shell file to use the travis path for export commands 
+
 #!/bin/bash
 $TRAVIS_BUILD_DIR/travis/install_gcloud.sh app-engine-python app-engine-go
 echo $sandbox_service_key | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
-ls /home/travis/google-cloud-sdk/ 
-ls /home/travis/
 export GOOGLE_APPLICATION_CREDENTIALS="${HOME}/gcloud-service-key.json" && export APPENGINE_DEV_APPSERVER="${HOME}/google-cloud-sdk/bin/dev_appserver.py" && source "${HOME}/google-cloud-sdk/path.bash.inc" && go test -v ./...
 
