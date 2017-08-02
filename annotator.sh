51c7e92c3af591f795b4494c3fbaddb2dc2796d0
Shelby Chen <shelbychen@shelbychen.nyc.corp.google.com>
2017-08-02 15:39:14 -0400

    fix annotator.sh

--- annotator.sh
+++ annotator.sh
@@ -5,5 +5,5 @@
 $TRAVIS_BUILD_DIR/travis/install_gcloud.sh app-engine-python app-engine-go
 echo $sandbox_service_key | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
-export GOOGLE_APPLICATION_CREDENTIALS="${HOME}/gcloud-service-key.json" && export APPENGINE_DEV_APPSERVER="${HOME}/google-cloud-sdk/bin/dev_appserver.py" && source "${HOME}/google-cloud-sdk/path.bash.inc" 
+export GOOGLE_APPLICATION_CREDENTIALS="${HOME}/gcloud-service-key.json" && export APPENGINE_DEV_APPSERVER="${HOME}/google-cloud-sdk/bin/dev_appserver.py" && source "${HOME}/google-cloud-sdk/path.bash.inc" && go test -v ./... 
 
