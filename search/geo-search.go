b3d5f472d36b6453b0da0f2d12656486026839b6
Shelby Chen <shelbychen@shelbychen.nyc.corp.google.com>
2017-08-07 18:01:14 -0400

    Adding linear search

---
+++ search/geo-search.go
@@ -0,0 +1,21 @@
+//Preforms linear search on a list of nodes for a geolocation
+package search
+
+import (
+	"bytes"
+	"errors"
+	"net"
+
+	"github.com/m-lab/annotation-service/parser"
+)
+
+// searches through array containing CSV file contents
+func searchList(list []parser.Node, ipLookUp string) (*parser.Node, error) {
+	userIP := net.ParseIP(ipLookUp)
+	for i := range list {
+		if bytes.Compare(userIP, list[i].LowRangeBin) >= 0 && bytes.Compare(userIP, list[i].HighRangeBin) <= 0 {
+			return &list[i], nil
+		}
+	}
+	return nil, errors.New("not found\n")
+}
---
+++ search/geo-search_test.go
@@ -0,0 +1,64 @@
+package search
+
+import (
+	"errors"
+	"net"
+	"strings"
+	"testing"
+
+	"github.com/m-lab/annotation-service/parser"
+)
+
+var listComp = []parser.Node{
+	parser.Node{
+		LowRangeBin:  net.IPv4(1, 0, 1, 0),
+		HighRangeBin: net.IPv4(1, 0, 3, 255),
+		CountryAbrv:  "CN",
+		CountryName:  "China",
+	},
+	parser.Node{
+		LowRangeBin:  net.IPv4(1, 0, 4, 0),
+		HighRangeBin: net.IPv4(1, 0, 7, 255),
+		CountryAbrv:  "AU",
+		CountryName:  "Australia",
+	},
+	parser.Node{
+		LowRangeBin:  net.IPv4(1, 0, 8, 0),
+		HighRangeBin: net.IPv4(1, 0, 15, 255),
+		CountryAbrv:  "CN",
+		CountryName:  "China",
+	},
+	parser.Node{
+		LowRangeBin:  net.IPv4(1, 0, 16, 0),
+		HighRangeBin: net.IPv4(1, 0, 31, 255),
+		CountryAbrv:  "JP",
+		CountryName:  "Japan",
+	},
+}
+
+func TestSearchList(t *testing.T) {
+	if checkSearch("1.0.4.0", listComp[1]) != nil {
+		t.Errorf("Search #1 FAILED")
+	}
+	if checkSearch("3.0.4.0", listComp[0]) == nil {
+		t.Errorf("Search #2 FAILED")
+	}
+	if checkSearch("1.0.4.1", listComp[1]) != nil {
+		t.Errorf("Search #3 FAILED")
+	}
+	if checkSearch("1.0.30.1", listComp[3]) != nil {
+		t.Errorf("Search #4 FAILED")
+	}
+
+}
+func checkSearch(IPLookUp string, ans parser.Node) error {
+	n, err := searchList(listComp, IPLookUp)
+	if err != nil {
+		output := strings.Join([]string{"expecting", ans.CountryName, "got: Node not found"}, "")
+		return errors.New(output)
+	} else if n.CountryName != ans.CountryName {
+		output := strings.Join([]string{"expecting", ans.CountryName, "got: ", n.CountryName}, "")
+		return errors.New(output)
+	}
+	return nil
+}
