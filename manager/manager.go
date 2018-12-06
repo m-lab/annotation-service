a5109f249a41f90f78e25b69f7a58196f1539766
Gregory Russell <gfr@google.com>
2018-12-06 15:07:54 -0500

    Add annotator loading logic

--- manager/manager.go
+++ manager/manager.go
@@ -14,6 +14,11 @@ var (
 	// ErrNilDataset is returned when CurrentAnnotator is nil.
 	ErrNilDataset = errors.New("CurrentAnnotator is nil")
 
+	// ErrPendingAnnotatorLoad is returned when a new annotator is requested, but not yet loaded.
+	ErrPendingAnnotatorLoad = errors.New("annotator is loading")
+
+	ErrAnnotatorLoadFailed = errors.New("unable to load annoator")
+
 	// A mutex to make sure that we are not reading from the CurrentAnnotator
 	// pointer while trying to update it
 	currentDataMutex = &sync.RWMutex{}
@@ -22,6 +27,72 @@ var (
 	// latest data for the annotator to search and reply with
 	CurrentAnnotator api.Annotator
 )
+
+// AnnotatorMap manages all loading of and already loaded Annotators
+type AnnotatorMap struct {
+	// Keys are date strings in YYYYMMDD format.
+	annotators map[string]api.Annotator
+	// Lock to be held when reading or writing the map.
+	mutex sync.RWMutex
+}
+
+// NOTE: Should only be called by checkAndLoadAnnotator.
+// Loads an annotator, and updates the pending map entry.
+// On entry, the calling goroutine should "own" the
+func (am *AnnotatorMap) loadAnnotator(dateString string) {
+	// On entry, this goroutine has exclusive ownership of the
+	// map entry, and the responsibility for loading the annotator.
+	var ann api.Annotator = nil
+	// TODO actually load the annotator and handle loading errors.
+
+	am.mutex.Lock()
+	defer am.mutex.Unlock()
+
+	ann, ok := am.annotators[dateString]
+	if !ok {
+		// TODO handle error
+	}
+	if ann != nil {
+		// TODO handle error
+	}
+	am.annotators[dateString] = ann
+}
+
+// This asynchronously attempts to set map entry to nil, and
+// if successful, proceeds to asynchronously load the new dataset.
+func (am *AnnotatorMap) checkAndLoadAnnotator(dateString string) {
+	go func() {
+		am.mutex.Lock()
+
+		_, ok := am.annotators[dateString]
+		if ok {
+			// Another goroutine is already responsible for loading.
+			am.mutex.Unlock()
+			return
+		} else {
+			// Place marker so that other requesters know it is loading.
+			am.annotators[dateString] = nil
+		}
+
+		// Drop the lock before attempting to load the annotator.
+		am.mutex.Unlock()
+		am.loadAnnotator(dateString)
+	}()
+}
+
+// Gets the named annotator, if already in the map.
+func (am *AnnotatorMap) GetAnnotator(dateString string) (api.Annotator, error) {
+	am.mutex.RLock()
+	defer am.mutex.RUnlock()
+
+	ann, ok := am.annotators[dateString]
+	if ok {
+		return ann, nil
+	} else {
+		am.checkAndLoadAnnotator(dateString)
+		return nil, ErrPendingAnnotatorLoad
+	}
+}
 
 // GetAnnotator returns the correct annotator to use for a given timestamp.
 func GetAnnotator(date time.Time) api.Annotator {
