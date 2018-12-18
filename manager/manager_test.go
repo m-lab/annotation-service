d6122efc3c3ccd08bf998b01f0f01d738a870829
Gregory Russell <gfr@google.com>
2018-12-18 10:39:49 -0500

    fix flaky test

--- manager/manager_test.go
+++ manager/manager_test.go
@@ -36,17 +36,18 @@ func TestAnnotatorMap(t *testing.T) {
 	// These are all fake names.
 	_, err := am.GetAnnotator(names[0])
 	if err != manager.ErrPendingAnnotatorLoad {
-		t.Error("Should be", manager.ErrPendingAnnotatorLoad)
+		t.Fatal("Should be", manager.ErrPendingAnnotatorLoad)
 	}
 
 	_, err = am.GetAnnotator(names[1])
 	if err != manager.ErrPendingAnnotatorLoad {
-		t.Error("Should be", manager.ErrPendingAnnotatorLoad)
+		t.Fatal("Should be", manager.ErrPendingAnnotatorLoad)
 	}
 
+	// This one should NOT kick off a load, because numPending already max.
 	_, err = am.GetAnnotator(names[2])
 	if err != manager.ErrPendingAnnotatorLoad {
-		t.Error("Should be", manager.ErrPendingAnnotatorLoad)
+		t.Fatal("Should be", manager.ErrPendingAnnotatorLoad)
 	}
 
 	// Wait for both annotator to be available.
@@ -68,6 +69,7 @@ func TestAnnotatorMap(t *testing.T) {
 	}(names[1])
 	wg.Wait()
 
+	// Verify that both annotators are now available.
 	ann, err := am.GetAnnotator(names[0])
 	if err != nil {
 		t.Error("Not expecting:", err)
@@ -76,36 +78,55 @@ func TestAnnotatorMap(t *testing.T) {
 		t.Error("Expecting non-nil annotator")
 	}
 
-	// Now try to load 2 more.  The second one should cause an eviction.
-	wg = &sync.WaitGroup{}
-	wg.Add(2)
-	go func(date string) {
+	ann, err = am.GetAnnotator(names[1])
+	if err != nil {
+		t.Error("Not expecting:", err)
+	}
+	if ann == nil {
+		t.Error("Expecting non-nil annotator")
+	}
+
+	// Verify that third is NOT available.  This kicks off loading.
+	_, err = am.GetAnnotator(names[2])
+	if err != manager.ErrPendingAnnotatorLoad {
+		t.Fatal("Should be", manager.ErrPendingAnnotatorLoad)
+	}
+
+	// Wait until it has loaded.
+	func(date string) {
 		err := errors.New("start")
 		for ; err != nil; _, err = am.GetAnnotator(date) {
 			time.Sleep(3 * time.Millisecond)
 		}
-		wg.Done()
 	}(names[2])
-	go func(date string) {
-		err := errors.New("start")
-		for ; err != nil; _, err = am.GetAnnotator(date) {
-			time.Sleep(3 * time.Millisecond)
-		}
-		wg.Done()
-	}(names[3])
-	wg.Wait()
+
+	// And now load the fourth.  This should cause synchronous eviction, and NOT cause loading.
+	_, err = am.GetAnnotator(names[3])
+	if err != manager.ErrPendingAnnotatorLoad {
+		t.Fatal("Should be", manager.ErrPendingAnnotatorLoad)
+	}
 
 	// Loading two more will have caused one to be evicted, so exactly one of these
 	// should no longer be loaded, and return an ErrPendingAnnotatorLoad.
+	// One of these checks will also trigger another load, but that is OK.
 	_, err0 := am.GetAnnotator(names[0])
 	_, err1 := am.GetAnnotator(names[1])
+	_, err2 := am.GetAnnotator(names[2])
 	switch {
-	case err0 == nil && err1 == nil:
+	case err0 == nil && err1 == nil && err2 == nil:
 		t.Error("One of the items should have been evicted")
-	case err0 == nil && err1 == manager.ErrPendingAnnotatorLoad:
-		// Good
-	case err0 == manager.ErrPendingAnnotatorLoad && err1 == nil:
-		// Good
+	case err0 == manager.ErrPendingAnnotatorLoad:
+		if err1 != nil || err2 != nil {
+			t.Error("More than one nil", err0, err1, err2)
+		}
+	case err1 == manager.ErrPendingAnnotatorLoad:
+		if err0 != nil || err2 != nil {
+			t.Error("More than one nil", err0, err1, err2)
+		}
+	case err2 == manager.ErrPendingAnnotatorLoad:
+		if err0 != nil || err1 != nil {
+			t.Error("More than one nil", err0, err1, err2)
+		}
 	default:
 		t.Error("Should have had exactly one ErrPending...", err0, err1)
 	}
