61b4aeac367af75e9a25c7febc039f211ebc3f65
yachang <yachang@users.noreply.github.com>
2018-12-19 10:13:11 -0500

    Update manager_test.go

--- manager/manager_test.go
+++ manager/manager_test.go
@@ -123,38 +123,34 @@ func TestE2ELoadMultipleDataset(t *testing.T) {
 		time string
 		res  string
 	}{
-		{"1.4.128.0", "1199145600", `annotator is loading`},
-		{"1.9.128.0", "1512086400", `annotator is loading`},
-		{"1.22.128.0", "1512086400", `annotator is loading`},
+		// This request needs a legacy binary dataset
+		{"1.4.128.0", "1199145600", `{"Geo":{"continent_code":"AS","country_code":"TH","country_code3":"THA","country_name":"Thailand","region":"40","city":"Bangkok","latitude":13.754,"longitude":100.501},"ASN":{}}`},
+		// This request needs a geolite2 dataset
+		{"1.9.128.0", "1512086400", `{"Geo":{"continent_code":"AS","country_code":"MY","country_name":"Malaysia","region":"14","city":"Kuala Lumpur","postal_code":"50400","latitude":3.149,"longitude":101.697},"ASN":{}}`},
+		// This request needs the latest dataset in the memory.
+		{"1.22.128.0", "1544400000", `{"Geo":{"continent_code":"AS","country_code":"IN","country_name":"India","region":"DL","city":"Delhi","postal_code":"110062","latitude":28.6667,"longitude":77.2167},"ASN":{}}`},
 	}
 	for _, test := range tests {
 		w := httptest.NewRecorder()
 		r := &http.Request{}
 		r.URL, _ = url.Parse("/annotate?ip_addr=" + url.QueryEscape(test.ip) + "&since_epoch=" + url.QueryEscape(test.time))
+		log.Println("Calling handler")
 		handler.Annotate(w, r)
-		body := w.Body.String()
-		if string(body) != test.res {
-			t.Errorf("\nGot\n__%s__\nexpected\n__%s__\n", body, test.res)
+		i := 30
+		for w.Result().StatusCode != http.StatusOK {
+			log.Println("Try again", w.Result().Status, w.Body.String())
+			i--
+			if i == 0 {
+				break
+			}
+			time.Sleep(1 * time.Second)
+			w = httptest.NewRecorder()
+			handler.Annotate(w, r)
 		}
-	}
 
-	time.Sleep(30 * time.Second)
-
-	tests2 := []struct {
-		ip   string
-		time string
-		res  string
-	}{
-		{"1.4.128.0", "1199145600", `{"Geo":{"continent_code":"AS","country_code":"TH","country_code3":"THA","country_name":"Thailand","region":"40","city":"Bangkok","latitude":13.754,"longitude":100.501},"ASN":{}}`},
-		{"1.9.128.0", "1512086400", `{"Geo":{"continent_code":"AS","country_code":"MY","country_name":"Malaysia","region":"14","city":"Kuala Lumpur","postal_code":"50400","latitude":3.149,"longitude":101.697},"ASN":{}}`},
-		{"1.22.128.0", "1512086400", `{"Geo":{"continent_code":"AS","country_code":"IN","country_name":"India","region":"DL","city":"Delhi","postal_code":"110062","latitude":28.6667,"longitude":77.2167},"ASN":{}}`},
-	}
-	for _, test := range tests2 {
-		w := httptest.NewRecorder()
-		r := &http.Request{}
-		r.URL, _ = url.Parse("/annotate?ip_addr=" + url.QueryEscape(test.ip) + "&since_epoch=" + url.QueryEscape(test.time))
-		handler.Annotate(w, r)
 		body := w.Body.String()
+		log.Println(body)
+
 		if string(body) != test.res {
 			t.Errorf("\nGot\n__%s__\nexpected\n__%s__\n", body, test.res)
 		}
