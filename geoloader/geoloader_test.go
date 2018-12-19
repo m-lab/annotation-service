4361be8b9bf1ead859485a2654ef6592d7826cee
Gregory Russell <gfr@google.com>
2018-12-19 14:43:33 -0500

    tweak test expectations

--- geoloader/geoloader_test.go
+++ geoloader/geoloader_test.go
@@ -29,25 +29,25 @@ func TestSelectArchivedDataset(t *testing.T) {
 
 	date2, _ := time.Parse("January 2, 2006", "March 7, 2014")
 	filename2, err := geoloader.SelectArchivedDataset(date2)
-	if filename2 != "Maxmind/2014/03/07/20140307T160000Z-GeoLiteCityv6.dat.gz" || err != nil {
+	if filename2 != "Maxmind/2014/03/07/20140307T160000Z-GeoLiteCity.dat.gz" || err != nil {
 		t.Errorf("Did not select correct dataset. Expected %s, got %s, %+v.",
-			"Maxmind/2014/03/07/20140307T160000Z-GeoLiteCityv6.dat.gz", filename2, err)
+			"Maxmind/2014/03/07/20140307T160000Z-GeoLiteCity.dat.gz", filename2, err)
 	}
 
 	// before the cutoff date.
 	date3, _ := time.Parse("January 2, 2006", "August 14, 2017")
 	filename3, err := geoloader.SelectArchivedDataset(date3)
-	if filename3 != "Maxmind/2017/08/08/20170808T080000Z-GeoLiteCityv6.dat.gz" || err != nil {
+	if filename3 != "Maxmind/2017/08/08/20170808T080000Z-GeoLiteCity.dat.gz" || err != nil {
 		t.Errorf("Did not select correct dataset. Expected %s, got %s, %+v.",
-			"Maxmind/2017/08/08/20170808T080000Z-GeoLiteCityv6.dat.gz", filename3, err)
+			"Maxmind/2017/08/08/20170808T080000Z-GeoLiteCity.dat.gz", filename3, err)
 	}
 
 	// after the cutoff date.
 	date4, _ := time.Parse("January 2, 2006", "August 15, 2017")
 	filename4, err := geoloader.SelectArchivedDataset(date4)
-	if filename4 != "Maxmind/2017/08/15/20170815T200946Z-GeoLite2-City-CSV.zip" || err != nil {
+	if filename4 != "Maxmind/2017/08/15/20170815T200728Z-GeoLite2-City-CSV.zip" || err != nil {
 		t.Errorf("Did not select correct dataset. Expected %s, got %s, %+v.",
-			"Maxmind/2017/08/15/20170815T200946Z-GeoLite2-City-CSV.zip", filename4, err)
+			"Maxmind/2017/08/15/20170815T200728Z-GeoLite2-City-CSV.zip", filename4, err)
 	}
 
 	// return the latest available dataset.
@@ -61,7 +61,7 @@ func TestSelectArchivedDataset(t *testing.T) {
 	// before the cutoff date, IPv6
 	date6, _ := time.Parse("January 2, 2006", "April 4, 2016")
 	filename6, err := geoloader.SelectArchivedDataset(date6)
-	if filename6 != "Maxmind/2016/03/08/20160308T080000Z-GeoLiteCityv6.dat.gz" || err != nil {
+	if filename6 != "Maxmind/2016/03/08/20160308T080000Z-GeoLiteCity.dat.gz" || err != nil {
 		t.Errorf("Did not select correct dataset. Expected %s, got %s, %+v.",
 			"Maxmind/2016/03/08/20160308T080000Z-GeoLiteCityv6.dat.gz", filename6, err)
 	}
