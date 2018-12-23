1e6babdcb2b12a2929683279fde34f1341093d95
Gregory Russell <gfr@google.com>
2018-12-23 16:26:10 -0500

    cleanup dead code

--- geoloader/geoloader.go
+++ geoloader/geoloader.go
@@ -4,25 +4,10 @@
 package geoloader
 
 import (
-	"log"
-
 	"github.com/m-lab/annotation-service/api"
 	"github.com/m-lab/annotation-service/geolite2"
 	"github.com/m-lab/annotation-service/legacy"
 )
-
-// PopulateLatestData will search to the latest Geolite2 files
-// available in GCS and will use them to create a new GeoDataset which
-// it will place into the global scope as the latest version. It will
-// do so safely with use of the currentDataMutex RW mutex. It it
-// encounters an error, it will halt the program.
-func GetLatestData() api.Annotator {
-	data, err := geolite2.LoadLatestGeolite2File()
-	if err != nil {
-		log.Fatal(err)
-	}
-	return data
-}
 
 func ArchivedLoader(filename string) (api.Annotator, error) {
 	if GeoLite2Regex.MatchString(filename) {
