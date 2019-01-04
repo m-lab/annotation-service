3e36d5b4cbd47ca0b657433374585bad232a438a
Greg Russell <gfr10598@gmail.com>
2019-01-04 17:35:55 -0500

    fix merge conflict

--- geoloader/internal/wrapper/wrapper_test.go
+++ geoloader/internal/wrapper/wrapper_test.go
@@ -1,13 +1,21 @@
-package geoloader_test
+package wrapper_test
 
 import (
 	"errors"
+	"log"
+	"sync"
 	"testing"
 	"time"
 
 	"github.com/m-lab/annotation-service/api"
 	"github.com/m-lab/annotation-service/geoloader"
+	"github.com/m-lab/annotation-service/geoloader/internal/wrapper"
 )
+
+func init() {
+	// Always prepend the filename and line number.
+	log.SetFlags(log.LstdFlags | log.Lshortfile | log.Lmicroseconds)
+}
 
 type fakeAnnotator struct {
 	api.Annotator
@@ -21,7 +29,7 @@ func (f *fakeAnnotator) Unload() {
 // This exercises all the basic functions of the wrapper.TestAnnWrapper
 // TODO - the next PR should add a test that checks for concurrency correctness and races.
 func TestAnnWrapper(t *testing.T) {
-	aw := geoloader.NewAnnWrapper()
+	aw := wrapper.New()
 
 	if !aw.GetLastUsed().Equal(time.Time{}) {
 		t.Error("incorrect last used")
@@ -37,7 +45,7 @@ func TestAnnWrapper(t *testing.T) {
 	aw.Unload()
 
 	fakeErr := errors.New("FakeError")
-	if aw.SetAnnotator(nil, fakeErr) != geoloader.ErrGoroutineNotOwner {
+	if aw.SetAnnotator(nil, fakeErr) != wrapper.ErrGoroutineNotOwner {
 		t.Error("Should have failed to set annotator")
 	}
 	if !aw.ReserveForLoad() {
@@ -110,4 +118,62 @@ func TestAnnWrapper(t *testing.T) {
 	if fakeAnn.unloadCount != 1 {
 		t.Error("Should have called Unload once", fakeAnn.unloadCount)
 	}
+}
+
+func race(t *testing.T, wg *sync.WaitGroup, aw *wrapper.AnnWrapper, fake *fakeAnnotator, stop chan struct{}) {
+	defer wg.Done()
+	for {
+		ann, err := aw.GetAnnotator()
+		if err == nil && ann == nil {
+			t.Fatal(err)
+			return
+		}
+
+		aw.GetLastUsed()
+
+		aw.Unload()
+
+		ann, err = aw.GetAnnotator()
+		if err == wrapper.ErrNilEntry {
+			if aw.ReserveForLoad() {
+				time.Sleep(time.Millisecond)
+				// This may sometimes fail if another goroutine has called unload.
+				err := aw.SetAnnotator(fake, nil)
+				if err != nil {
+					if err == wrapper.ErrGoroutineNotOwner {
+						// Annotator was unloaded by another goroutine.
+					} else {
+						t.Fatal(err)
+						return
+					}
+				}
+			}
+		}
+
+		select {
+		case <-stop:
+			return
+		default:
+		}
+	}
+}
+
+// This test should be run with -race
+func TestRaces(t *testing.T) {
+	aw := wrapper.New()
+	done := make(chan struct{})
+	wg := sync.WaitGroup{}
+
+	fake := fakeAnnotator{}
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go race(t, &wg, &aw, &fake, done)
+	}
+
+	time.Sleep(2 * time.Second)
+	close(done)
+	wg.Wait()
+	log.Println(fake.unloadCount)
+
 }
