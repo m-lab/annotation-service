3d21bb88446c027c895b836ac639d8cbf98ef10f
Greg Russell <gfr10598@gmail.com>
2019-01-11 14:36:43 -0500

    Add request metrics

--- handler/handler_test.go
+++ handler/handler_test.go
@@ -36,7 +36,7 @@ func TestAnnotate(t *testing.T) {
 		res  string
 	}{
 		{"1.4.128.0", "625600", `{"Geo":{"region":"ME","city":"Not A Real City","postal_code":"10583","latitude":42.1,"longitude":-73.1},"ASN":{}}`},
-		{"This will be an error.", "1000", "Invalid request"},
+		{"This will be an error.", "1000", "invalid IP address"},
 	}
 	// TODO - make and use an annotator generator
 	manager.CurrentAnnotator = &geolite2.GeoDataset{
@@ -198,7 +198,7 @@ func TestBatchAnnotate(t *testing.T) {
 	}{
 		{
 			body: "{",
-			res:  "Invalid Request!",
+			res:  "unexpected end of JSON input",
 			alt:  "",
 		},
 		{
