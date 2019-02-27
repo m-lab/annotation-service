e1eab74f831492eee73bffee2d5e73f10706a582
Peter Boothe <pboothe@google.com>
2019-02-27 11:47:16 -0500

    Reify labeled metrics

--- metrics/metrics_test.go
+++ metrics/metrics_test.go
@@ -3,9 +3,15 @@ package metrics_test
 import (
 	"testing"
 
+	"github.com/m-lab/annotation-service/api/v2"
+	"github.com/m-lab/annotation-service/metrics"
 	"github.com/m-lab/go/prometheusx"
 )
 
 func TestPrometheusMetrics(t *testing.T) {
+	api.RequestTimeHistogram.WithLabelValues("x")
+	metrics.RequestTimeHistogramUsec.WithLabelValues("x", "x")
+	metrics.ErrorTotal.WithLabelValues("x")
+	metrics.RejectionCount.WithLabelValues("x")
 	prometheusx.LintMetrics(t)
 }
