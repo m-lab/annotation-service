9f57faf5c7fe7e70f855c9ff8e71f498d54bac35
zoltan.kauker@doctusoft.com <zoltan.kauker@doctusoft.com>
2019-03-05 10:10:13 +0100

    fix the expected dataset date for the V6 test as well

--- asn/asn_test.go
+++ asn/asn_test.go
@@ -60,7 +60,7 @@ func TestAnnotateV4(t *testing.T) {
 }
 
 func TestAnnotateV6(t *testing.T) {
-	datasetsTime := time.Date(2018, 1, 1, 8, 0, 0, 0, time.UTC)
+	datasetsTime := time.Date(2018, 1, 1, 0, 0, 0, 0, time.UTC)
 	ann := getAnnotatorForDay(t, false, datasetsTime)
 
 	// test simple ASN
