ac586cfb0e1652721a2bd8fee9775adf83f79464
zoltan.kauker@doctusoft.com <zoltan.kauker@doctusoft.com>
2019-03-06 11:47:21 +0100

    updated the date filter on the ASN files per Ya Chang request: v4 from 2009/02, v6 from 2018/01 based on slack communication CPU count and memory need increased to test if we can run this service as-is

--- geoloader/geoloader-asn.go
+++ geoloader/geoloader-asn.go
@@ -1,10 +1,14 @@
 package geoloader
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"regexp"
 	"time"
+
+	"github.com/m-lab/annotation-service/asn"
+	"github.com/m-lab/annotation-service/loader"
 
 	"cloud.google.com/go/storage"
 	"github.com/m-lab/annotation-service/api"
@@ -13,6 +17,11 @@ import (
 var (
 	asnRegexV4 = regexp.MustCompile(`RouteViewIPv4/\d{4}/\d{2}/routeviews-(oix|rv2)-\d{8}-\d{4}\.pfx2as\.gz`) // matches to the IPv4 RouteView datasets
 	asnRegexV6 = regexp.MustCompile(`RouteViewIPv6/\d{4}/\d{2}/routeviews-rv6-\d{8}-\d{4}\.pfx2as\.gz`)       // matches to the IPv6 RouteView datasets
+
+	asnV4StartTime = time.Date(2009, time.Month(2), 1, 0, 0, 0, 0, time.UTC)
+	asnV6StartTime = time.Date(2018, time.Month(1), 1, 0, 0, 0, 0, time.UTC)
+
+	errNeededLoadingDate = errors.New("Befoore needed loading date")
 )
 
 // UseSpecificASNDate is for unit tests to narrow the datasets to load from GCS to date that can be matched to the date part regexes.
@@ -42,7 +51,7 @@ func ASNv4Loader(
 	loader func(*storage.ObjectAttrs) (api.Annotator, error)) api.CachingLoader {
 	return newCachingLoader(
 		func(file *storage.ObjectAttrs) error {
-			return filter(file, asnRegexV4, time.Time{})
+			return asnFilterFrom(file, asnRegexV4, asnV4StartTime)
 		},
 		loader,
 		api.RouteViewPrefix)
@@ -53,8 +62,26 @@ func ASNv6Loader(
 	loader func(*storage.ObjectAttrs) (api.Annotator, error)) api.CachingLoader {
 	return newCachingLoader(
 		func(file *storage.ObjectAttrs) error {
-			return filter(file, asnRegexV6, time.Time{})
+			return asnFilterFrom(file, asnRegexV6, asnV6StartTime)
 		},
 		loader,
 		api.RouteViewPrefix)
+}
+
+func asnFilterFrom(file *storage.ObjectAttrs, r *regexp.Regexp, from time.Time) error {
+	baseFilename := loader.GetGzBase(file.Name)
+	fileTime, err := asn.ExtractTimeFromASNFileName(baseFilename)
+	if err != nil {
+		return err
+	}
+
+	if from.After(*fileTime) {
+		return errNeededLoadingDate
+	}
+
+	if !r.MatchString(file.Name) {
+		return errNoMatch
+	}
+
+	return nil
 }
