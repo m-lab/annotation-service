94f338371521267d7d6fa73bc5dd55b6cd574ced
yachang <changya@gmail.com>
2019-04-02 15:56:04 -0400

    fmt

--- main.go
+++ main.go
@@ -71,7 +71,7 @@ func main() {
 		memoryless.Config{Expected: *updateInterval, Min: *minInterval, Max: *maxInterval})
 
 	http.HandleFunc("/status", Status)
-        http.HandleFunc("/updateDatasets", updateMaxmindDatasets)
+	http.HandleFunc("/updateDatasets", updateMaxmindDatasets)
 
 	handler.InitHandler()
 	prometheusx.MustStartPrometheus(":9090")
